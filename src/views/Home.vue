977b7b627ac07ca2629cc29a13dd9331616198d8
Denis Chernov <zede169778@gmail.com>
2018-02-15 15:46:24 +0500

    Исправлены начальные шаблоны(языки препроцессоров/ расстановка точек с запятыми)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
-<template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!--suppress HtmlUnknownTarget -->
+<template lang="pug">
+  .home
+    img(src='../assets/logo.png')
+    helloworld(msg='Welcome to Your Vue.js App')
 </template>
 
 <script lang="ts">
